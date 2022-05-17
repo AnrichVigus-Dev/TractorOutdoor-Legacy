900c20503f878342f151f26eb537c26db504bb44
AnrichMark1 <83713185+AnrichMark1@users.noreply.github.com>
2022-05-17 19:02:25 +0200

    upadet
    update

--- step2-b.php
+++ step2-b.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
     <title>Step 2</title>
 </head>
 <body>
@@ -13,12 +14,12 @@
 
 <h3>Select the region</h3>
 
-<button>Cape Town</button>
-<button>Durban</button>
-<button>Johannesburg</button>
+<button id="btn-cpt">Cape Town</button>
+<button id="btn-durban">Durban</button>
+<button id="btn-jhb">Johannesburg</button>
 
 <!-- CAPE TOWN -->
-<section id="region-capetown" style="background-color:#f2f2f2; width:500px; padding:20px; margin-top:20px">
+<section id="region-capetown">
 <h3>Cape Town</h3>
 <div class="region-selction-cont" style="display:flex; width:800px; gap:30px">
     <div class="region-selction-inner">
@@ -41,7 +42,7 @@
 </section>
 
 <!-- DURBAN -->
-<section id="region-durban" style="background-color:#e0e0e0; width:500px; padding:20px;  margin-top:20px">
+<section id="region-durban" >
 <h3>Durban</h3>
 <div class="region-selction-cont" style="display:flex; width:800px; gap:30px">
     <div class="region-selction-inner">
@@ -64,7 +65,7 @@
 </section>
 
 <!-- JOHANNESBURG -->
-<section id="region-johannesburg" style="background-color:#cccaca; width:500px; padding:20px;  margin-top:20px">
+<section id="region-johannesburg"  >
 <h3>Johannesburg</h3>
 <div class="region-selction-cont" style="display:flex; width:800px; gap:30px">
     <div class="region-selction-inner">
@@ -89,6 +90,44 @@
 
 
 <!-- Javascript link -->
-<script src="./js/main.js"></script>
+<script>
+        // Cape Town
+const targetCpt = document.getElementById("region-capetown");
+const btn = document.getElementById("btn-cpt");
+    btn.onclick = function () {
+      if (targetCpt.style.display !== "none") {
+        targetCpt.style.display = "none";
+      } else {
+        targetCpt.style.display = "block";
+      }
+    };
+
+
+// Durban
+const targetDurban = document.getElementById("region-durban");
+const btn = document.getElementById("btn-durban");
+    btn.onclick = function () {
+      if (targetDurban.style.display !== "none") {
+        targetDurban.style.display = "none";
+      } else {
+        targetDurban.style.display = "block";
+      }
+    };
+
+
+// Johannesburg
+const targetDiv = document.getElementById("region-johannesburg");
+const btn = document.getElementById("btn-jhb");
+    btn.onclick = function () {
+      if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+      } else {
+        targetDiv.style.display = "block";
+      }
+    };
+
+
+</script>
+
 </body>
 </html>
