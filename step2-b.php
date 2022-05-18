a418da9d59102e537c0044b8a1c1265c8ff69554
AnrichMark1 <83713185+AnrichMark1@users.noreply.github.com>
2022-05-18 09:07:31 +0200

    17 May - night updates
    Changed the step2-b and style file

--- step2-b.php
+++ step2-b.php
@@ -15,8 +15,7 @@
 <h3>Select the region</h3>
 
 <button id="btn-cpt">Cape Town</button>
-<button id="btn-durban">Durban</button>
-<button id="btn-jhb">Johannesburg</button>
+<button id="btn-drb">Durban</button>
 
 <!-- CAPE TOWN -->
 <section id="region-capetown">
@@ -41,45 +40,24 @@
 
 </section>
 
-<!-- DURBAN -->
-<section id="region-durban" >
+
+
+<!-- CAPE TOWN -->
+<section id="region-durban">
 <h3>Durban</h3>
 <div class="region-selction-cont" style="display:flex; width:800px; gap:30px">
     <div class="region-selction-inner">
- 
+       
             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
             <label for="vehicle1">Location 1</label><br>
     </div>
     <div class="region-selction-inner">
-  
+    
             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
             <label for="vehicle1">Location 2</label><br>
     </div>
     <div class="region-selction-inner">
-   
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-            <label for="vehicle1"> Location 3</label><br>
-    </div>    
-</div> 
 
-</section>
-
-<!-- JOHANNESBURG -->
-<section id="region-johannesburg"  >
-<h3>Johannesburg</h3>
-<div class="region-selction-cont" style="display:flex; width:800px; gap:30px">
-    <div class="region-selction-inner">
-  
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-            <label for="vehicle1">Location 1</label><br>
-    </div>
-    <div class="region-selction-inner">
- 
-            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-            <label for="vehicle1">Location 2</label><br>
-    </div>
-    <div class="region-selction-inner">
- 
             <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
             <label for="vehicle1">Location 3</label><br>
     </div>    
@@ -89,9 +67,13 @@
 
 
 
+
+
+
 <!-- Javascript link -->
 <script>
-        // Cape Town
+
+// Cape Town
 const targetCpt = document.getElementById("region-capetown");
 const btn = document.getElementById("btn-cpt");
     btn.onclick = function () {
@@ -103,29 +85,16 @@ const btn = document.getElementById("btn-cpt");
     };
 
 
-// Durban
-const targetDurban = document.getElementById("region-durban");
-const btn = document.getElementById("btn-durban");
-    btn.onclick = function () {
-      if (targetDurban.style.display !== "none") {
-        targetDurban.style.display = "none";
+// durban
+const targetdrb = document.getElementById("region-durban");
+const btn2 = document.getElementById("btn-drb");
+    btn2.onclick = function () {
+      if (targetdrb.style.display !== "none") {
+        targetdrb.style.display = "none";
       } else {
-        targetDurban.style.display = "block";
+        targetdrb.style.display = "block";
       }
     };
-
-
-// Johannesburg
-const targetDiv = document.getElementById("region-johannesburg");
-const btn = document.getElementById("btn-jhb");
-    btn.onclick = function () {
-      if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-      } else {
-        targetDiv.style.display = "block";
-      }
-    };
-
 
 </script>
 
